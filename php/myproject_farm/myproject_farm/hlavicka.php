b5a5082411fce30febe862d8f575fc63c5b1dad6
evamagyarova <eva.magyarova1992@gmail.com>
2023-10-29 14:11:49 +0100

    Aktualizácia

--- php/myproject_farm/myproject_farm/hlavicka.php
+++ php/myproject_farm/myproject_farm/hlavicka.php
@@ -3,6 +3,7 @@
     <title>Levandule | Jur nad Hronom</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="media.css">
 </head>
 
 <body class="pozadie">
